cf5f71f22b4e81671465f451337dedcce7c4e59f
unknown <muzzone777@gmail.com>
2022-02-22 05:17:32 +0400

    threading update

--- client/client.py
+++ client/client.py
@@ -1,24 +1,35 @@
 import socket
 import os.path
 
-filename = "console.jpg"
-size = os.path.getsize(filename)
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 5001))
-print(size)
-client.send(f"{size}".encode('utf-8'))
 
-file = open(filename, mode = "rb")
-data = file.read(2048)
-while data:
-    client.send(data)
-    data= file.read(2048)    
-file.close()
+def start_client():
+    nick = input ("Ваше имя: ")
+    while True:
+        filename = input("Введите название файла: ")
+        size = os.path.getsize(filename)
+        client.send(f"{size}".encode('utf-8'))
+        file = open(filename, mode = "rb")
+        print("Original image is opened")
+        data = file.read(2048)
+        print("1 package sent")
+        count = 1
+        while data:
+            client.send(data)
+            count+=1
+            print(f"{count} package sent")
+            data= file.read(2048)    
+        file.close()
+        print('Origin image is closed')
+        file = open(f'{nick}_res_image.jpg', mode = "wb")
+        print('res_image is created')
+        data = client.recv(2048)
+        while data:
+            file.write(data)
+            data = client.recv(2048)
+        file.close()
+        print('File closed')
 
-file = open('res_image.jpg', mode = "wb")
-data = client.recv(2048)
-while data:
-    file.write(data)
-    data = client.recv(2048)
-file.close()
-  
+if __name__ == '__main__':
+    start_client()
